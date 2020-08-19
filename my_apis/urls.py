dcefecd77ede1568162fcacac419af52452469b4
Dilip Joshi <dilipjoshis98@gmail.com>
2020-08-19 16:38:30 +0530

    token added

--- my_apis/urls.py
+++ my_apis/urls.py
@@ -5,7 +5,7 @@ from rest_framework.authtoken import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    #path('admin/', admin.site.urls),
     path('corona/', include('corona.urls')),
     path('students/',include('students.urls')),
     path('gettoken/',views.obtain_auth_token,name='api-token-auth')
